d1e26045147b51044b847aa754ea857fadf54443
Harshal Tupe <harshaltupe12@gmail.com>
2024-03-03 20:07:42 +0530

    minor setup changes adding cross origin access control

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require("express")
 const app = express();
 const connectDB = require("./db/connect");
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 app.get("/", (req, res)=>{
     res.send("Hello Backend is Working");
 })
